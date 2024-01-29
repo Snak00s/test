9d908357cfef15f79cd16021986b6ac3904cefb6
Snak00s <tom.snak00s@gmail.com>
2024-01-29 11:46:14 +0100

    c'est des essais tu vois

--- test2.py
+++ test2.py
@@ -1,8 +1,14 @@
-def f(phrase) :
-    P=[]
-    M=[]
-    for k in phrase :
-        P.append(k)
-    return P
+import numpy as np
+import matplotlib.pyplot as plt
 
-print(f("bonjour"))
+x = np.array([1, 2, 6, 8])
+y = np.array([3, 8, 1, 10])
+x2 = np.array([1, 2, 3, 1])
+y2 = np.array([3, 9, 5, 12])
+
+plt.plot(x,y,'-', c='r', lw='5')
+plt.plot(x2,y2,'-', c='b', lw='5')
+plt.title('La taille du flop', loc='right')
+plt.xlabel('temps')
+plt.ylabel('taille du flop')
+plt.show()
